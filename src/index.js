15671860bd8582125a7031a27bf7be7b401f2a0c
Courtney Macgregor <cjoss90@hotmail.com>
2025-01-05 15:09:39 +1000

    Done assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault()
+    const taskDescription = document.querySelector("#new-task-description").value
+    newListItem(taskDescription)
+    form.reset()
+  })
 });
+
+function newListItem(create_task) {
+  let li = document.createElement("li")
+  let btn = document.createElement("button")
+  btn.addEventListener("click", handleDelete)
+  btn.textContent = "done"
+  li.textContent = `${create_task} ` 
+  li.appendChild(btn)
+  document.querySelector("#tasks").appendChild(li)
+
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
